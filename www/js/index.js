062d39219de89c0c6e8b013202cc5d69b49520de
Hannu <hannu.kikkas@gmail.com>
2015-05-09 13:09:50 +0300

    1.0.2

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.	See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.	 See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
+	
+	mapContainerId: 'map',
+	
 	// Application Constructor
 	initialize: function() {
 		this.bindEvents();
@@ -33,17 +18,49 @@ var app = {
 	// The scope of 'this' is the event. In order to call the 'receivedEvent'
 	// function, we must explicitly call 'app.receivedEvent(...);'
 	onDeviceReady: function() {
-		app.receivedEvent('deviceready');
+	   // app.receivedEvent('deviceready');
+	   navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
 	},
-	// Update DOM on a Received Event
-	receivedEvent: function(id) {
-		var parentElement = document.getElementById(id);
-		var listeningElement = parentElement.querySelector('.listening');
-		var receivedElement = parentElement.querySelector('.received');
+ 
+	onSuccess: function(position){
+		var longitude = position.coords.longitude;
+		var latitude = position.coords.latitude;
+		var latLong = new google.maps.LatLng(latitude, longitude);
+ 
+		var mapOptions = {
+			center: latLong,
+			zoom: 13,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+ 
+		var map = new google.maps.Map(document.getElementById(this.mapContainerId), mapOptions);
+	
+		var marker = new google.maps.Marker({
+			  position: latLong,
+			  map: map,
+			  title: 'my location'
+		  });
+	},
+	
+	fakeTheMap : function() {
+		var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+		var mapOptions = {
+			zoom: 12,
+			center: myLatlng
+		}
+		var map = new google.maps.Map(document.getElementById(this.mapContainerId), mapOptions);
 
-		listeningElement.setAttribute('style', 'display:none;');
-		receivedElement.setAttribute('style', 'display:block;');
-
-		console.log('Received Event: ' + id);
-	}
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'Hello World!'
+		});
+	},
+	
+	onError: function(error){
+		alert("the code is " + error.code + ". \n" + "message: " + error.message);
+	},
 };
+ 
+//google.maps.event.addDomListener(window, 'load', app.fakeTheMap());
+app.initialize();
