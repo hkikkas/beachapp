eca948529e044f2c859839a3336cda95b614ed1a
Hannu <hannu.kikkas@gmail.com>
2015-05-09 15:09:47 +0300

    1.0.5

--- www/js/index.js
+++ www/js/index.js
@@ -72,5 +72,5 @@ var app = {
 	}
 };
  
-google.maps.event.addDomListener(window, 'load', app.fakeTheMap());
-//app.initialize();
+//google.maps.event.addDomListener(window, 'load', app.fakeTheMap());
+app.initialize();
